5444d3430354435c05ad2ac36cf1ee80b73b98d3
Navingaayathri Mahalingam <gaayathrioneness@gmail.com>
2021-10-23 00:38:50 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -2,25 +2,20 @@
 #define STATS_H 
 
 #include <math.h>
+
 #ifdef NAN
 /* NAN is supported */
 #endif
 
 struct Stats 
 {
-    float average;
+	float average;
     float min ;
     float max;
 };
 
-extern  Stats compute_statistics(const float* numberset, int setlength);
-Stats compute_statistics(const int numberset, int setlength) ;
-//typedef void (*alerter_funcptr)();
-//extern void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
-//extern void  emailAlerter();
-//extern void  ledAlerter();
-extern int emailAlertCallCount;
-extern int ledAlertCallCount;
-#endif
+extern Stats compute_statistics(const float* numberset, int setlength);
+extern Stats compute_statistics(const int numberset, int setlength) ;
 
+#endif
 
